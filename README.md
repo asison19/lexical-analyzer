d2f757c8233de0e822f5fc34998505d79c444d84
Andrew Sison <andrewsison19@gmail.com>
2019-09-28 20:55:50 -0700

    Worked on making installation easier.

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 If there is a weird character on the top of the produced java file from the jflex spec, such as ï»¿ save endcoding as UTF-8.  
-Input (*.in) files of the Toy "code" that needs to be compiled needs to be converted and encoded in UTF-8. The following endcodings won't work:  
+Input (.in) files of the Toy "code" that needs to be compiled needs to be converted and encoded in UTF-8. The following endcodings won't work:  
 ANSI, UTF-8-BOM, UCS-2-BE-BOM, UCS-2-LE-BOM  
-ANSI can work with text(*.txt) files.
+Both ANSI, and UTF-8 can work with text(.txt) files.
 
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,24 +1,35 @@
+/*
+ * Created by: Andrew Sison, and Zach Martin
+ * This code does lexical analysis for the Toy programming language.
+ */
+
 package main;
 import lexicalAnalyzer.*;
-import token.Token;
-
-import java.io.BufferedWriter;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.io.Reader;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
-		String inFile = "inputs/SampleOriginalUTF8.in";
-		int x = 0;
-		Reader reader = new FileReader( inFile );
+		String infile = "../inputs/SampleOriginalUTF8.in";
+		FileReader reader;
+		try {
+			infile = args[0];
+			System.out.println("Using file: " + infile);
+			reader = new FileReader(infile);
+		} catch(IndexOutOfBoundsException e) {
+			infile = "inputs/SampleOriginalUTF8.in";
+			System.out.println("No inputted file detected, using default file: " + infile);
+			reader = new FileReader(infile);
+		}
+		
 		ToyLexer lexer = new ToyLexer(reader);
-		while(x  < 1000) { // TODO read all
-		lexer.yylex();
-		x++;	
-		}
-		lexer.outputTrie();
-		}
+		int count = 0;
+		System.out.println("********** Start of lexical analysis **********");
+		do {
+			count++;
+		} while (lexer.yylex() != null);
+		
+		System.out.println("\n********** End of lexical analysis **********");
+		System.out.println("There were " + count +" tokens in the file: " + infile);
+	}
 }
